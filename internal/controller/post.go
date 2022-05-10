7165606d565414ae9a8142fa37f3c4a8e2d810f4
Firdavs <firdavs792@gmail.com>
2022-05-10 16:32:55 +0500

    feat: add post controller layer to project

---
+++ internal/controller/post.go
@@ -0,0 +1,67 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/zfirdavs/another-rest/internal/entity"
+	"github.com/zfirdavs/another-rest/internal/service"
+	"github.com/zfirdavs/another-rest/pkg/errors"
+)
+
+type PostController interface {
+	GetPosts(w http.ResponseWriter, r *http.Request)
+	AddPost(w http.ResponseWriter, r *http.Request)
+}
+
+type handler struct {
+	service service.PostService
+}
+
+func NewHandler(service service.PostService) *handler {
+	return &handler{service}
+}
+
+func (h *handler) GetPosts(w http.ResponseWriter, r *http.Request) {
+	posts, err := h.service.FindAll(r.Context())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(errors.ServiceError{Message: "error getting posts"})
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(posts)
+}
+
+func (h *handler) AddPost(w http.ResponseWriter, r *http.Request) {
+	var post entity.Post
+	err := json.NewDecoder(r.Body).Decode(&post)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(errors.ServiceError{Message: "error unmarshalling post"})
+		return
+	}
+
+	ctx := r.Context()
+
+	err = h.service.Validate(ctx, &post)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(errors.ServiceError{Message: err.Error()})
+		return
+	}
+
+	err = h.service.Create(ctx, &post)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(errors.ServiceError{Message: "error creating post"})
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(post)
+
+}
